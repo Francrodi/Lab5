6076ffd4d466e7dd6f2ed456cb55bd9fa8220326
Federico Nicolas Aguilera Tourn <federico.aguilera@fing.edu.uy>
2018-06-02 15:01:06 -0300

    Delete menu and move to main

--- Lab5.cpp
+++ Lab5.cpp
@@ -1,13 +1,78 @@
 #include <iostream>
+
 #include "h/Checks.h"
 #include "h/Const.h"
-#include "h/Menu.h"
 
 using namespace std;
 
+int getIntInput() {
+	int input;
+	cin >> input;
+	return input;
+}
+
+string getStringInput() {
+	string input;
+	cin >> input;
+	return input;
+}
+
+void printStart() {
+	cout << "Bienvenidos al menu" << endl;
+}
+
+void printMainMenu() {
+	cout << "[1] INICIAR SESION" << endl;
+	cout << "[2] DEFAULT XXXXXXXX" << endl;
+	cout << "[3] DEFAULT XXXXXXXX" << endl;
+	cout << "[4] DEFAULT XXXXXXXX" << endl;
+	cout << "[5] DEFAULT XXXXXXXX" << endl;
+	cout << "[6] DEFAULT XXXXXXXX" << endl;
+	cout << "[7] DEFAULT XXXXXXXX" << endl;
+	cout << "[8] DEFAULT XXXXXXXX" << endl;
+	cout << "[9] DEFAULT XXXXXXXX" << endl;
+	cout << "[10] SALIR" << endl;
+	cout << "Ingrese opcion: ";
+}
+
+void iniciarSesion() {
+	cout << "Ingrese usuario y contraseña" << endl;
+
+	cout << "USUARIO: " << endl;
+	string user = getStringInput();
+
+	cout << "CONTRASEÑA: " << endl;
+	string pass = getStringInput();
+}
+
 int main() {
 
-	selectMenu();
+	printStart();
+
+	int seleccion = 0;
+
+	do {
+		printMainMenu();
+
+		seleccion = getIntInput();
+
+		switch(seleccion) {
+		case 1:
+			iniciarSesion();
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 10:
+			cout << "Terminando programa" << endl;
+			cout << "-------------------" << endl;
+			break;
+		default:
+			cout << "Porfavor ingrese una selección valida\n" << endl;
+			break;
+		}
+	} while (seleccion != 10);
 
 	return 0;
 }
--- h/Menu.h
+++
@@ -1,9 +0,0 @@
-#ifndef H_MENU_H_
-#define H_MENU_H_
-
-void printStart();
-void printMainMenu();
-int getInput();
-void selectMenu();
-
-#endif
